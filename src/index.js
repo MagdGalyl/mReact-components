6653d33f8dc4a225235fa1510218b99655b21f24
“Magd <majdgalyl@gmail.com>
2021-11-29 00:53:29 +0200

    Added ApprovalCard Outer Component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 
 if (module.hot) {
@@ -12,26 +13,29 @@ const App = () => {
   return (
     <div className="ui container comments">
       
-      <CommentDetail 
-      author = "Sam" 
-      timeAgo = "Today at 08:18 PM" 
-      content = "yo yo yo" 
-      avatar = {faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail 
+        author = "Sam" 
+        timeAgo = "Today at 08:18 PM" 
+        content = "yo yo yo" 
+        avatar = {faker.image.avatar()}
+        />
+      </ApprovalCard>
 
-      <CommentDetail 
-      author = "Alex" 
-      timeAgo = "Today at 18:08 AM"  
-      content = "yo yo yo" 
-      avatar = {faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail 
+        author = "Alex" 
+        timeAgo = "Today at 18:08 AM"  
+        content = "yo yo yo" 
+        avatar = {faker.image.avatar()}
+        />
+      </ApprovalCard>
 
-      <CommentDetail 
-      author = "Jane" 
-      timeAgo = "Yesterday at 08:18 PM" 
-      content = "yo yo yo" 
-      avatar = {faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail 
+        author = "Jane" timeAgo = "Yesterday at 08:18 PM" content = "yo yo yo" avatar = {faker.image.avatar()} />
+      </ApprovalCard>
+
     </div>
   );
 };
